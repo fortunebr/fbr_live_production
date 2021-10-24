70f4caac201636b1d1f98e02c59b4195c496e5d9
kalaLokia <kalalokia@gmail.com>
2021-10-24 16:07:50 +0530

    New changes

--- run.py
+++ run.py
@@ -20,109 +20,80 @@ data = {
     21: (15665, 1015),
 }
 
-def getClockImage(hour:int):
+
+class Production:
+    def __init__(self, production, fg, phour):
+        self.production = production
+        self.fg = fg
+        self.last_hour = phour
+
+
+def getClockImage(hour: int):
     match hour:
-        case  1 |  13 :
+        case  1 | 13:
             return "https://i.imgur.com/QXIxBcr.png"
-        case   2|14   :
+        case   2 | 14:
             return "https://i.imgur.com/sO7GiQh.png"
-        case   3|15   :
+        case   3 | 15:
             return "https://i.imgur.com/FmZrupJ.png"
-        case   4|16   :
+        case   4 | 16:
             return "https://i.imgur.com/zNeOJSL.png"
-        case   5|17   :
+        case   5 | 17:
             return "https://i.imgur.com/S4UPkPd.png"
-        case   6|18   :
+        case   6 | 18:
             return "https://i.imgur.com/lC2E845.png"
-        case   7|19   :
+        case   7 | 19:
             return "https://i.imgur.com/wcj8ESf.png"
-        case   8|20   :
+        case   8 | 20:
             return "https://i.imgur.com/GVsFAgG.png"
-        case   9|21   :
+        case   9 | 21:
             return "https://i.imgur.com/SPHh9BK.png"
-        case   10|22   :
+        case   10 | 22:
             return "https://i.imgur.com/rQDK31v.png"
-        case   11|23   :
+        case   11 | 23:
             return "https://i.imgur.com/e8WYgmU.png"
-        case   12|0   :
+        case   12 | 0:
             return "https://i.imgur.com/oqb1oQz.png"
         case _:
             return "https://listimg.pinclipart.com/picdir/s/195-1954098_24-hour-clock-spiral-free-printable-template-download.png"
 
+
 def hourlyLogDisplay(data: dict) -> str:
     """For displaying hourly log"""
 
-    end_border = """
-|-------|-------|-------|
-"""
-    data_string = ""
-    table = end_border
+    data_string = "```\n"
     for key, item in data.items():
-        # table += f"| {key-1:02d}-{key:02d} |"
-        # table += f" {item[1]}" + " " * (6 - len(str(item[1])))
-        # table += "|"
-        # table += f" {item[0]}" + " " * (6 - len(str(item[0])))
-        # table += "|" + end_border
-
-
         data_string += f"08-{key:02d}  :  {item[0]}"
         data_string += " " * (8 - len(str(item[0])))
         data_string += f"+{item[1]}\n"
-    return data_string
+    return data_string + "```"
 
 
 def randomColor() -> int:
-    num = random.randint(0,9)
+    num = random.randint(0, 9)
     match num:
         case 0:
-            return 5763719 # Green
+            return 5763719  # Green
         case 1:
-            return 16705372 # Yellow
+            return 16705372  # Yellow
         case 2:
-            return 15548997 # Red
+            return 15548997  # Red
         case 3:
-            return 15418782 # Rose
+            return 15418782  # Rose
         case 4:
-            return 5793266 # Purple
+            return 5793266  # Purple
         case 5:
-            return 1146986 # Dark Green
+            return 1146986  # Dark Green
         case 6:
-            return 15277667 # Pink
+            return 15277667  # Pink
         case 7:
-            return 15105570 # Orange
+            return 15105570  # Orange
         case _:
-            return 3447003 # Blue
+            return 3447003  # Blue
 
 
-def webhook_discord(production, date, history, fg_prod) -> None:
+def webhook_discord(embed) -> None:
     url = "https://discordapp.com/api/webhooks/897150490536718398/_ay4C-asZPGNa6TFnTVBT-IrqlJUlafC4Y4pld2y6O8NL2x5sr69CWb1ezIPEVc6Sy1d"
-
-    date_string = date.strftime("%b %d, %Y  %I:%M %p")
-    embed = {
-        "embeds": [
-            {
-                "color": f"{randomColor()}",
-                "thumbnail": {
-		                        "url": f"{getClockImage(date.hour)}",
-                },
-                "fields": [
-                    {
-                        "name": "Production",
-                        "value": f"{production} pairs | {fg_prod} cs",
-                    },
-
-                    {
-                        "name": "Hourly Report",
-                        "value": f"```arm\n{history}```",
-                    },
-                ],
-                "footer": {
-                    "text": f"{date_string}",
-                    "icon_url": "https://i.imgur.com/7SwrwqC.jpg",
-                },
-            }
-        ]
-    }
 
     try:
         res = requests.post(url, json=embed)
@@ -132,13 +103,91 @@ def webhook_discord(production, date, history, fg_prod) -> None:
     except Exception as e:
         print(f"Failed to send to discord webhook\n{e}")
 
-    print(f"Embed sent. #{res.status_code}")
+
+def discord_embed_h(date: datetime.datetime, prod: Production, data: dict):
+    date_string = date.strftime("%b %d, %Y  %I:%M %p")
+    average_hprod = int(sum([v[1] for v in data.values()]) / len(data))
+
+    embed = {
+        "embeds": [
+            {
+                "color": f"{randomColor()}",
+                "thumbnail": {
+                    "url": "https://i.imgur.com/e22h9tf.png",
+                },
+                "fields": [
+                    {
+                        "name": "Achieved",
+                        "value": f"{prod.production} pairs | {prod.fg} cs",
+                    },
+                    {
+                        "name": "Last Hour",
+                        "value": f"{prod.last_hour} pairs",
+                        "inline": True
+                    },
+                    {
+                        "name": "Average",
+                        "value": f"{average_hprod} pairs/hour",
+                        "inline": True
+                    },
+                ],
+                "footer": {
+                    "text": f"{date_string}",
+                    "icon_url": "https://i.imgur.com/7SwrwqC.jpg",
+                },
+            }
+        ]
+    }
+    return embed
+
+
+def discord_embed_summary(date: datetime.datetime, prod: Production, data: dict):
+    date_string = date.strftime("%b %d, %Y  %I:%M %p")
+    average_hprod = int(sum([v[1] for v in data.values()]) / len(data))
+
+    embed = {
+        "embeds": [
+            {
+                "color": f"{randomColor()}",
+                "thumbnail": {
+                    "url": "https://i.imgur.com/e22h9tf.png",
+                },
+                "author": {
+                    "name": f"{now.strftime('%A - %b %d, %Y')}"
+                },
+                "fields": [
+                    {
+                        "name": "Total Achieved",
+                        "value": f"**{prod.production}** pairs | **{prod.fg}** cs",
+                    },
+                    {
+                        "name": "Highest",
+                        "value": f"**{prod.last_hour}** pairs \n11am-12pm",
+                        "inline": True
+                    },
+                    {
+                        "name": "Average",
+                        "value": f"{average_hprod} pairs/hour",
+                        "inline": True
+                    },
+                    {
+                        "name": "Report",
+                        "value": hourlyLogDisplay(data),
+                    }
+                ],
+                "timestamp": f"{date.utcnow()}",
+                "footer": {
+                    "text": f"Fortune Br",
+                    "icon_url": "https://i.imgur.com/7SwrwqC.jpg",
+                },
+            }
+        ]
+    }
+    return embed
 
 
 if __name__ == "__main__":
     now = datetime.datetime.now()
-
-    webhook_discord(
-        production=prod, date=now, history=hourlyLogDisplay(data=data), fg_prod=fg_prod
-    )
-
+    production = Production(production=prod, fg=fg_prod, phour=1122)
+    embed = discord_embed_summary(now, production, data)
+    webhook_discord(embed=embed)
