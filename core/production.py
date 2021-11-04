fb748feb106d4958c0a9fa1afe6dec65cedc3164
kalaLokia <kalalokia@gmail.com>
2021-11-04 09:57:50 +0530

    [format] Code splitted into modules ! Testing pending

---
+++ core/production.py
@@ -0,0 +1,69 @@
+"""
+Production information.
+
+"""
+
+
+import datetime
+from typing import Iterable
+
+
+class Production:
+    """Model for storing hourly production details."""
+
+    def __init__(
+        self,
+        date=datetime.datetime.now(),
+        time=datetime.datetime.now(),
+        achieved: int = 0,
+        fg: int = 0,
+        phour: int = 0,
+    ):
+
+        self.time: datetime.datetime = time
+        """Current hour datetime object."""
+        self.date: datetime.datetime = date
+        """Production day datetime object"""
+        self.achieved: int = achieved
+        """Production achieved upto this hour."""
+        self.fg: int = fg
+        """FG Production upto this hour."""
+        self.phour: int = phour
+        """Production on this hour."""
+
+    @property
+    def hour_string(self) -> str:
+        from_hour = (self.time - datetime.timedelta(hours=1)).hour
+        to_hour = self.time.hour
+        return f"{from_hour:02d}-{to_hour:02d}"
+
+    @property
+    def date_string(self):
+        return self.time.strftime("%b %d, %Y  %I:%M %p")
+
+    @property
+    def time_string(self):
+        return self.time.strftime("%I:%M %p")
+
+
+def averageHourlyProduction(productions: Iterable[Production]) -> int:
+    """Returns average production per hour."""
+
+    return int(sum([p.phour for p in productions]) / len(productions))
+
+
+def generateProductionSummary(data: dict[datetime.datetime, Production]) -> dict:
+    """Returns production summary report"""
+
+    summary = {}
+    summary["top"] = max(data.values(), key=lambda p: p.phour)
+    summary["detail"] = ""
+    for p in data.values():
+        summary["detail"] += f"{p.hour_string}  :  {p.achieved}"
+        summary["detail"] += " " * (8 - len(str(p.achieved)))
+        summary["detail"] += f"+{p.phour}\n"
+
+    # productions = list(data.values()) # Sort for getting top5, last5
+    # productions.sort(key=lambda x: x.phour)
+
+    return summary
---
+++ core/utils.py
@@ -0,0 +1,116 @@
+"""
+Utilities
+
+"""
+
+
+import configparser
+import datetime
+import pickle
+from typing import TYPE_CHECKING, Tuple
+
+
+if TYPE_CHECKING:
+    from .production import Production
+
+
+root = "C:/fbr_production/"
+
+
+def getDailyProductionDate(cur_datetime) -> Tuple[datetime.datetime]:
+    """Get production hours.
+
+    8am to 8am (next day)
+    """
+
+    # New day production logging start at 9am (8am-9am)
+    logtime = datetime.time(9)
+
+    if cur_datetime.time() < logtime:
+        sday = cur_datetime.date() - datetime.timedelta(days=1)
+    else:
+        sday = cur_datetime.date()
+
+    eday = sday + datetime.timedelta(days=1)
+    time_period = datetime.time(8, 0)
+    start_date = datetime.datetime.combine(sday, time_period)
+    end_date = datetime.datetime.combine(eday, time_period)
+    return (start_date, end_date)
+
+
+def logMessage(msg) -> None:
+    """Program execution failure/exception logging"""
+
+    global root
+
+    with open(root + "log.txt", "a+") as f:
+        f.write(f"{datetime.datetime.now()}  {msg}\n")
+
+
+def load_configuration():
+    """Load application's configurations"""
+
+    global root
+
+    config = configparser.ConfigParser(interpolation=None)
+    exists = config.read(root + "config.ini")
+    wh = {}
+    if exists:
+        try:
+            connection_str = (
+                r"Driver={ODBC Driver 17 for SQL Server};"
+                rf'Server={config["SQL Server"]["SERVER"]};'
+                rf'Database={config["SQL Server"]["DATABASE"]};'
+                rf'uid={config["SQL Server"]["UID"]};'
+                rf'pwd={config["SQL Server"]["PWD"]};'
+                r"Integrated Security=false;"
+            )
+            if config.has_option("WEBHOOK", "DISCORD"):
+                wh["DISCORD"] = config.get("WEBHOOK", "DISCORD")
+            if config.has_option("WEBHOOK", "DISCORD_DAILY"):
+                wh["DISCORD_DAILY"] = config.get("WEBHOOK", "DISCORD_DAILY")
+            if config.has_option("WEBHOOK", "SLACK"):
+                wh["SLACK"] = config.get("WEBHOOK", "SLACK")
+            if config.has_option("WEBHOOK", "GOOGLE"):
+                wh["GOOGLE"] = config.get("WEBHOOK", "GOOGLE")
+
+            if not wh.keys():
+                logMessage("Cannot find a webhook to send report to!")
+
+            return connection_str, wh
+        except KeyError as e:
+            logMessage(f'Required key "{e.args[0]}" not found in configurations.')
+
+        except Exception as e:
+            logMessage(f"Unknown exception occured.\n{e}")
+    else:
+        logMessage("Configuration file missing.")
+        return None, wh
+
+
+def loadHourlyProductionLog() -> dict[datetime.datetime, "Production"]:
+    """Get hourly logging from previously saved local file"""
+
+    global root
+    hourly_log = {}
+
+    try:
+        with open(root + "production.pickle", "rb") as f:
+            hourly_log = pickle.load(f)
+    except FileNotFoundError or EOFError:
+        pass
+    except Exception as e:
+        logMessage(f"Failed to load previous production log.\n{e}")
+
+    return hourly_log
+
+
+def saveHourlyProductionLog(data: dict) -> None:
+    """Saves the current latest log in the file"""
+
+    global root
+    try:
+        with open(root + "production.pickle", "w+b") as f:
+            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+    except Exception as e:
+        logMessage(f"Failed to save current production log. \n{e}")
